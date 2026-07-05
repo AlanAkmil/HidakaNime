cf09b465f3e240aef581e0c5674a83d0993b4d40
AlanAkmil <alansubagyo058@gmail.com>
2026-07-06 02:40:17 +0700

    Upload api/detail.js

--- api/detail.js
+++ api/detail.js
@@ -1,10 +1,10 @@
 const IFILMScraper = require('../lib/scraper');
 module.exports = async (req, res) => {
   try {
-    const { slug } = req.query;
+    const { slug, type } = req.query;
     if (!slug) return res.status(400).json({ success: false, error: 'Parameter "slug" wajib diisi' });
     const scraper = new IFILMScraper();
-    const result = await scraper.getDetail(slug);
+    const result = type === 'anime' ? await scraper.getAnimeDetail(slug) : await scraper.getDetail(slug);
     res.status(200).json(result);
   } catch (err) {
     res.status(500).json({ success: false, error: err.message });
